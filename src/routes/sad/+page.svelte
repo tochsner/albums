0e60945f0245be6c00586667efa05cf50f012ed5
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-06-09 11:58:18 +0200

    Add play button morphing.

--- src/routes/sad/+page.svelte
+++ src/routes/sad/+page.svelte
@@ -169,14 +169,37 @@
 					clip-path="url(#clip-shape)"
 				/>
 
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke="white"
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
 					stroke-width="2"
-					transform="translate(43, 43) scale(7)"
-					d="M5.25 5.653c0-.856.17-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
-				/>
+					stroke="currentColor"
+					class="size-6 origin-center scale-60 text-white transition-opacity ease-out"
+					opacity={isPlaying ? 0.0 : 1.0}
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+					/>
+				</svg>
+
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="2"
+					stroke="currentColor"
+					class="size-6 origin-center scale-60 text-white transition-opacity ease-in"
+					opacity={isPlaying ? 1.0 : 0.0}
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z"
+					/>
+				</svg>
 			</svg>
 		</button>
 
