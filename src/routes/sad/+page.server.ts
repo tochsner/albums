1a7e331ddeb630787d5fd8a95d9eb7ec618e2f15
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-06-10 08:22:52 +0200

    Change fetching logic.

--- src/routes/sad/+page.server.ts
+++ src/routes/sad/+page.server.ts
@@ -3,32 +3,18 @@ import type { PageServerLoad } from './$types';
 import { supabase } from '$lib/supabaseClient';
 
 export const load: PageServerLoad = async () => {
-	const today = new Date();
-	today.setHours(0, 0, 0, 0);
-	const todayISOString = today.toISOString();
-
-	console.log(todayISOString);
-
 	const { data } = await supabase
 		.from('Album')
 		.select('*')
-		.gte('created_at', todayISOString)
-		.order('created_at', { ascending: true })
+		.order('created_at', { ascending: false })
 		.limit(1);
-
-	console.log('Fetched');
-
 	const album = data[0];
-
-	console.log(album);
 
 	const songs = await supabase
 		.from('Song')
 		.select('*')
 		.eq('albumTitle', album.title)
 		.eq('artist', album.artist);
-
-	console.log(songs);
 
 	return {
 		...album,
