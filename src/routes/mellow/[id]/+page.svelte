bcf7e46ef5976150e2d72a6db5a76ef4de16f191
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-06-12 14:46:01 +0200

    Hide arrow down for last song.

--- src/routes/mellow/[id]/+page.svelte
+++ src/routes/mellow/[id]/+page.svelte
@@ -247,6 +247,7 @@
 			stroke-width="2.5"
 			stroke="currentColor"
 			class="mt-12 mb-4 size-6 text-white opacity-70"
+			class:invisible={idx === songs.length - 1}
 		>
 			<path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
 		</svg>
